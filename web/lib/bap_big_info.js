53292541cefa79f79533299140db6aaee5c4e20d
Lars Knudsen <larsgk@gmail.com>
2024-10-03 00:04:40 +0200

    Fix big info capture

--- web/lib/bap_big_info.js
+++ web/lib/bap_big_info.js
@@ -9,13 +9,13 @@ export const parse_big_info = (big_info_buf) => {
 
         result.num_bis = dv.getUint8(0);
         result.sub_evt_count = dv.getUint8(1);
-        result.iso_interval = dv.getUint16(2);
+        result.iso_interval = dv.getUint16(2, true);
         result.burst_number = dv.getUint8(4);
         result.offset = dv.getUint8(5);
         result.rep_count = dv.getUint8(6);
-        result.max_pdu = dv.getUint16(7);
-        result.sdu_interval = dv.getUint32(9);
-        result.max_sdu = dv.getUint16(13);
+        result.max_pdu = dv.getUint16(7, true);
+        result.sdu_interval = dv.getUint32(9, true);
+        result.max_sdu = dv.getUint16(13, true);
         result.phy = dv.getUint8(15);
         result.framing = dv.getUint8(16);
         result.encryption = dv.getUint8(17) == 1;
